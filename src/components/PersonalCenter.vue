5850b806c8a4ed0c9d326264c6c3c5f6b7566523
LWF0824 <985579124@qq.com>
2022-04-23 18:23:10 +0800

    登录注册

---
+++ src/components/PersonalCenter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="hello"></div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+#hello{
+  min-height:1000px;
+  background-image: url(../../src/assets/background.png);
+  background-size:100% 100%;
+  background-attachment:fixed;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,27 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
+import Register from '@/components/Register'
+import PersonalCenter from '@/components/PersonalCenter'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/Login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/Register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/',
+      name: 'PersonalCenter',
+      component: PersonalCenter
     }
   ]
 })
