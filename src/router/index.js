bebb24c760141249379cb74cc9bcdaec00dcf415
LXX987 <2590168411@qq.com>
2022-04-23 18:26:22 +0800

    首页

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
+import PsychologyHome from '@/components/PsychologyHome'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/PsychologyHome',
+      name: 'PsychologyHome',
+      component: PsychologyHome
     }
   ]
 })
