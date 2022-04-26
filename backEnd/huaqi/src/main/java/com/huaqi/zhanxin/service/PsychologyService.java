428dac942c1bae511695bd17731648e2ed01c2b4
LXX987 <2590168411@qq.com>
2022-04-26 10:24:57 +0800

    Psychology

--- backEnd/huaqi/src/main/java/com/huaqi/zhanxin/service/PsychologyService.java
+++ backEnd/huaqi/src/main/java/com/huaqi/zhanxin/service/PsychologyService.java
@@ -11,6 +11,8 @@ public interface PsychologyService {
 
     public List<PsychologyBean> getHistory(int userId);
 
+    public List<PsychologyBean> getResult(int userId);
+
     public int recordScore(int userId, Timestamp testTime, int score1, int score2,int score3, int score4, int score5, int score);
 
 }
