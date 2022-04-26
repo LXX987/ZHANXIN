b1e6836ea6fe42ce613b28e425f830b9e4ee9bf8
1602347312 <1602347312@qq.com>
2022-04-26 16:52:57 +0800

    1

--- backEnd/huaqi/src/main/java/com/huaqi/zhanxin/mapper/VideoMapper.java
+++ backEnd/huaqi/src/main/java/com/huaqi/zhanxin/mapper/VideoMapper.java
@@ -11,13 +11,13 @@ import java.util.List;
 @Mapper
 @Repository
 public interface VideoMapper {
-    @Select("select * from Video_Info")
-    List<VideoInfo> selectAll();
+    @Select("select * from Video_Info where video_type=#{video_type}")
+    List<VideoInfo> selectAllByType(@Param("video_type") String type);
 
-    @Insert("insert into Video_Info(video_name,video_time,video_url,video_intro)" +
-            " values (#{video_name},#{video_time},#{video_url},#{video_intro})")
+    @Insert("insert into Video_Info(video_name,video_time,video_url,video_intro,video_cover,video_type)" +
+            " values (#{video_name},#{video_time},#{video_url},#{video_intro},#{video_cover},#{video_type})")
     void insert(@Param("video_name")String video_name, @Param("video_time")LocalDateTime video_time, @Param("video_url")String video_url,
-                @Param("video_intro")String video_intro);
+                @Param("video_intro")String video_intro, @Param("video_type") String video_type, @Param("video_cover") String video_cover);
 
     @Delete("delete from Video_Info where video_id=#{id}")
     Integer deleteOneById(Integer id);
