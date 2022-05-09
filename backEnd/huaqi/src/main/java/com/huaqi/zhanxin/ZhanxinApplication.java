60a9bf7cf0f2ef435db68e0ae1a44eca921bb70d
LXX987 <2590168411@qq.com>
2022-05-09 22:24:11 +0800

    Psychology

--- backEnd/huaqi/src/main/java/com/huaqi/zhanxin/ZhanxinApplication.java
+++ backEnd/huaqi/src/main/java/com/huaqi/zhanxin/ZhanxinApplication.java
@@ -2,6 +2,7 @@ package com.huaqi.zhanxin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ZhanxinApplication {
